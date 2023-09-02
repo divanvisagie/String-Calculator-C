4f3e55c2f110ccbd0938c79ca9158fdaf3fdaf1a
Divan visagie <me@dvisagie.com>
2023-09-02 13:00:46 +0200

    Extract main calculator

---
+++ src/calculator.c
@@ -0,0 +1,69 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "delim_parser.h"
+
+void replace_string(char* str, const char* search, const char* replace) {
+    char* pos;
+    int search_len = strlen(search);
+    int replace_len = strlen(replace);
+
+    pos = strstr(str, search);
+
+    while (pos != NULL) {
+        memmove(pos + replace_len, pos + search_len, strlen(pos + search_len) + 1);
+        memcpy(pos, replace, replace_len);
+        pos = strstr(pos + replace_len, search);
+    }
+}
+
+/**
+ * Add function that takes a string of numbers and returns the sum of those numbers.
+ * 
+ * @param input_ptr - string of numbers to add
+ * @return sum of numbers
+ * @retval >= 0 - sum of numbers
+ * @retval -1 - negative number found
+ * @retval -2 - error finding delimiter
+ */
+int add(char* input_ptr) {
+    if (input_ptr == NULL || strcmp(input_ptr, "") == 0) {
+        return 0;
+    }
+
+    DelimInfo info = find_delim_str(input_ptr);
+    char* delims[] = {NULL};
+    delims[0] = info.delim;
+
+    if (info.error != NULL) {
+        printf("Error finding delim %s\n", info.error);
+        return -2;
+    }
+     
+    for (int i = 0; i < 1; i++) {
+        if (delims[i] == NULL) {
+            continue;
+        }
+        replace_string(info.remaining, delims[i], "\n");
+    }
+
+    int sum = 0;
+    char *tofree, *string, *token;
+    // replace all string delims with newline character
+    string = strdup(info.remaining);
+    tofree = string;
+    while ((token = strsep(&string, "\n")) != NULL) {
+        int num = atoi(token);
+        if (num < 0) {
+            return -1;
+        }
+        sum += num;
+    }
+
+    return sum;
+}
+
--- src/delim_parser.h
+++ src/delim_parser.h
@@ -1,3 +1,6 @@
+#ifndef DELIM_PARSER_H
+#define DELIM_PARSER_H
+
 typedef struct {
     char* delim;
     char* remaining;
@@ -5,3 +8,6 @@ typedef struct {
 } DelimInfo;
 
 DelimInfo find_delim_str(const char* input);
+
+#endif
+
